4f0cd109d1faea25f0fe2fcf001e939b00e30f41
raschy <raschy@gmx.de>
2023-04-27 10:58:15 +0200

    Prototype beta

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ class Deyeidc extends utils.Adapter {
 		//
 		this.idc = new idcCore();
 		this.client = new net.Socket();
-		//this.client.setTimeout(30000);	//deactiviert
+		this.client.setTimeout(20000);	//deactiviert
 		// because [W505] setTimeout found in "main.js", but no clearTimeout detected in AdapterCheck
 		// -----------------  Timeout variables -----------------
 		this.sync_milliseconds = 60000; // 1min
@@ -39,6 +39,7 @@ class Deyeidc extends utils.Adapter {
 		this.req = 0;
 		this.CalcValues = [];
 		this.setWatchPoints = false;
+		this.resetCounter = 0;
 	}
 
 	/**
@@ -83,6 +84,8 @@ class Deyeidc extends utils.Adapter {
 			await this.connectionHandler();
 			// request
 			await this.requestData();
+			// subscribe Contraol
+			this.subscribeStates(this.config.logger + '.' + 'Control.PowerSet');
 		}
 	}
 
@@ -94,9 +97,13 @@ class Deyeidc extends utils.Adapter {
 	async onStateChange(id, state) {
 		if (state) {
 			// The state was changed
-			this.log.debug(`state ${id} has changed`);
-			this.updateData(await this.computeData(id, state));
-
+			this.log.debug(`state ${id} has changed to ${state.val}`);
+			if (id.indexOf('PowerSet') > 1) {
+				await this.setPower(id, state);
+			} else {
+				//await this.computeData(id, state);	// zum Test
+				this.updateData(await this.computeData(id, state));
+			}
 		} else {
 			// The state was deleted
 			this.log.debug(`state ${id} deleted`);
@@ -112,6 +119,7 @@ class Deyeidc extends utils.Adapter {
 		this.client.on('connect', () => {
 			this.log.debug(`connected`);
 			this.connectionActive = true;
+			this.resetCounter = 0;
 			this.setState('info.connection', this.connectionActive, true);
 		});
 
@@ -127,6 +135,8 @@ class Deyeidc extends utils.Adapter {
 			this.connectionActive = false;
 			this.setState('info.connection', this.connectionActive, true);
 			if (err) this.log.debug(`Error during connection ${err.message}`);
+			// Counter for PowerReset
+			this.powerReset(err);
 		});
 
 		this.client.on('end', () => {
@@ -184,8 +194,25 @@ class Deyeidc extends utils.Adapter {
 		if (!this.connectionActive) this.connect();
 		//
 		if (req > this.numberRegisterSets - 1) return;
-		const request = this.idc.request_frame(req);
+		const request = this.idc.requestFrame(req, this.idc.modbusFrame(req));
 		//console.log(`Anfrage Registersatz: ${(req + 1)} > ${this.idc.toHexString(request)}`); // human readable
+		this.client.write(request);
+
+	}
+
+	async setPower(id, state) {
+		//console.log(`[setPower] <${id}> ${state.val}`);
+		const req = 0;
+		const powerControlRegister = 40;
+		const data = [];
+		if (state.val > 100) {
+			data[0] = 100;
+		} else {
+			data[0] = state.val;
+		}
+		console.log(`[setPower] >> ${data[0]}`);
+		const request = this.idc.requestFrame(req, this.idc.modbusWriteFrame(powerControlRegister, data));
+		//console.log(`Write Registersatz: ${(req)} > ${this.idc.toHexString(request)}`); // human readable
 		this.client.write(request);
 
 	}
@@ -196,62 +223,126 @@ class Deyeidc extends utils.Adapter {
 		const basedir = id.substring(0, pos);
 		const name = id.substring(pos);
 
-		const jsonResult = []; // leeres Array
+		const jsonResult = [];
+		const varCompute = [];
+
 		if (state) {
-
+			//console.log(`[computeData ###] ${basedir} ${name} ${state.val} `);
 			const changes = this.CalcValues.filter(calc => calc.values.includes(name));
-			//console.log(`[computeData] <${changes.length}> ${JSON.stringify(changes)}`);
-
+			//console.log(`[computeData #1#] <${changes.length}> ${JSON.stringify(changes)}`);
+			//
 			for (let i = 0; i < changes.length; i++) {
-				let product = 1;
+				let computeResult = 0;
 				for (let j = 0; j < changes[i].values.length; j++) {
+					//console.log(`[computeData ##] ${i} ${j} <${changes[i].values[j]}>`);
 					const state = await this.getStateAsync(basedir + changes[i].values[j]);
-					//console.log(`[computeData ##] ${i} ${j} ## ${basedir} <${changes[i].values[j]}> ${JSON.stringify(state)} `);
 
 					if (typeof state?.val === 'number') {
-						const value = state?.val;
-						product *= value;
-						if (j == changes[i].values.length - 1) {
-							const product_hr = product.toFixed(changes[i].factor);
-							const jsonObj = { key: changes[i].key, value: product_hr, unit: changes[i].unit, name: changes[i].name };
-							//console.log(`Ergebnis = ${product_hr}  ${JSON.stringify(jsonObj)}`);
-							jsonResult.push(jsonObj);
-						}
+						varCompute[j] = state?.val;
+						//console.log(`[readCompute #1] Wert (${j}) <${changes[i].values[j]}> => ${varCompute[j]}`);
+					} else {
+						varCompute[j] = parseFloat(changes[i].values[j].replace(/[^0-9.]/g, ''));
+						//console.log(`[readCompute #1] Kons (${j}) <${changes[i].values[j]}> => ${varCompute[j]}`);
+					}
+					//
+					switch (changes[i].operation) {
+						case '+':
+							computeResult = varCompute[0] + varCompute[1];
+							break;
+						case '-':
+							computeResult = varCompute[0] - varCompute[1];
+							break;
+						case '*':
+							computeResult = varCompute[0] * varCompute[1];
+							break;
+						case '/':
+							computeResult = varCompute[0] / varCompute[1];
+							break;
 					}
 				}
-			}
+				//console.log(`[computeResult #${i}#]  <${varCompute[0]}> ${changes[i].operation} <${varCompute[1]}> = ${computeResult}`);
+				//const product_hr = (computeResult * 10 ** -changes[i].factor).toFixed(changes[i].factor);
+				const product_hr = (computeResult * 10 ** -changes[i].factor).toFixed(2);
+				//const product_hr = product.toFixed(changes[i].factor);
+
+				const jsonObj = { key: changes[i].key, value: product_hr, unit: changes[i].unit, name: changes[i].name };
+				console.log(`Ergebnis = ${product_hr}  ${JSON.stringify(jsonObj)}`);
+				jsonResult.push(jsonObj);
+			} // for
 		}
 		return (jsonResult);
 	}
 
 	async readComputeAndWatch() {
 		if (this.setWatchPoints) return;
+		const basedir = this.namespace + '.' + this.config.logger + '.';
 		const jsonResult = [];
+		const varCompute = [];
+		const watchStates = [];
 		const computeConfig = this.config.computes;
 		if (computeConfig && Array.isArray(computeConfig)) {
-			console.log(`[readCompute ##1] ${JSON.stringify(computeConfig)}`);
-			computeConfig.forEach(e => {
-				console.log(`[readCompute ##2] ${e.value1} ${e.value2}`);
-				if (e.value1 != 'none' && e.value1.length < 2) {
-					this.log.warn(`[watchStates] Value1 "${e.value1}" is not valid!`);
-					return;
-				}
-				if (e.value2 != 'none' && e.value2.length < 2) {
-					this.log.warn(`[watchStates] Value2 "${e.value2}" is not valid!`);
-					return;
-				}
-				this.log.debug(`[watchStates] set to ${e.value1} and ${e.value2}`);
-				this.subscribeStates(this.config.logger + '.' + e.value1);
-				this.subscribeStates(this.config.logger + '.' + e.value2);
+			//const watchStates = [];
+			for (const obj of computeConfig) {
+				const values = [];
+				const response = mathOperation(obj.values);
+				const operation = response?.operation;
+				const position = response?.position;
+				//console.log(`[readCompute #1] Das Zeichen (${operation}) wurde gefunden an Position ${position}.`);
+				varCompute[0] = obj.values.slice(0, position).trim();
+				varCompute[1] = obj.values.slice(position + 1).trim();
+				values.push(varCompute[0]);
+				values.push(varCompute[1]);
 				//
-				const values = JSON.parse('["' + e.value1 + '","' + e.value2 + '"]');
-				console.log(`[readCompute Values]  ${JSON.stringify(values)}`);
-				const jsonString = { values: values, key: e.key, name: e.name, unit: e.unit, factor: e.factor };
+				for (let i = 0; i < 2; i++) {
+					const state = await this.getStateAsync(basedir + values[i]);
+					//console.log(`[readCompute ##2] ${values[i]} ${JSON.stringify(state)} ${i} `);
+					if (state) { // != null
+						//console.log(`[readCompute ##3] ${values[i]} ${JSON.stringify(state)}`);
+						if (!watchStates.includes(values[i])) {
+							watchStates.push(values[i]);
+							//console.log(`[readCompute ###2] ${values[i]} ${JSON.stringify(watchStates)}`);
+						}
+					}
+				}
+				const jsonString = { values: values, operation: operation, key: obj.key, name: obj.name, unit: obj.unit, factor: obj.factor };
 				jsonResult.push(jsonString);
-			});
-			//console.log(`[readCompute ##3]  ${JSON.stringify(jsonResult)}`);
-			this.CalcValues = jsonResult;
+			}
+			//console.log(`[readCompute #2] ${JSON.stringify(watchStates)}`);
+			for (const watch of watchStates) {
+				this.subscribeStates(this.config.logger + '.' + watch);
+				this.log.debug(`[watchStates] set to ${watch}`);
+			}
 			this.setWatchPoints = true;
+			//console.log(`[readCompute #3]  ${JSON.stringify(jsonResult)}`);
+			this.CalcValues = jsonResult;
+		}
+		function mathOperation(computeString) {
+			const defMathOperators = ['+', '-', '*', '/'];
+			let position;
+			for (let i = 0; i < defMathOperators.length; i++) {
+				const zeichen = defMathOperators[i];
+				position = computeString.indexOf(zeichen);
+				if (position > 0) {
+					//console.log(`Das Zeichen (${i}) '${computeString[position]}' wurde gefunden an Position ${position}.`);
+					const jsonString = { operation: computeString[position], position: position };
+					return jsonString;
+				}
+			}
+		}
+	}
+
+	powerReset(err) {
+		// Counter for PowerReset
+		if (err.message.indexOf('EHOSTUNREACH') > 1) {
+			this.resetCounter++;
+			this.log.debug(`[powerReset] Counter ${this.resetCounter}`);
+		}
+		if (this.resetCounter == 29) {
+			const jsonResult = [];
+			const jsonString = { key: 'Apo_t1', value: 0, unit: 'x', name: 'y' };
+			jsonResult.push(jsonString);
+			this.updateData(jsonResult);
+			this.log.debug(`[powerReset] Power resettet`);
 		}
 	}
 
@@ -300,7 +391,7 @@ class Deyeidc extends utils.Adapter {
 		}
 	}
 
-	// create object für datavalues
+	// create object vor datavalues
 	async createDPsForInstances() {
 		const _loggerSn = String(this.config.logger);
 		await this.setObjectNotExistsAsync(_loggerSn, {
@@ -343,7 +434,7 @@ class Deyeidc extends utils.Adapter {
 			this.internDataReady = false;
 			return;
 		}
-		if (this.config.logger < 3900000000) {
+		if (this.config.logger < 2.1 * 10 ** 9) {
 			this.log.error(`Logger number seems to be wrong [${this.config.logger}] .`);
 			this.internDataReady = false;
 			return;
