3c90650c49a724bcd3c7a1dd9094f8daceb4cb0b
raschy <raschy@gmx.de>
2024-07-11 09:40:01 +0200

    Domain IP allowed

--- main.js
+++ main.js
@@ -581,8 +581,9 @@ class Deyeidc extends utils.Adapter {
 		return;
 		//
 		function validateIP(ip) {
-			const pattern = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-			return pattern.test(ip);
+			const ipPattern = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+			const domainPattern = /^(?!:\/\/)([a-zA-Z0-9-_]+\.)+[a-zA-Z]{2,}$/;
+			return ipPattern.test(ip) || domainPattern.test(ip);
 		}
 	}
 
